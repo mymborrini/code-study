6885e6177d42bef4c446abf7bfe1051157ad76c2
boma01 <mattia.borrini@e-globe-solutions.com>
2020-12-31 15:35:41 +0100

    Hibernate manage trigger and default value from database

--- caveatemptor/src/model/Item.java
+++ caveatemptor/src/model/Item.java
@@ -5,15 +5,21 @@ import java.util.Date;
 import java.util.Set;
 import java.util.HashSet;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
 /*
 This is an example to show that a lot of hibernate annotations does not exist in JPA
 */
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+
 import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.DiscriminatorFormula;
 
-@Entity
+// This name is use in HQL if there is a conflict between java classes maybe with the
+// same name but in different packages
+@Entity(name = "AuctionItem")
 @Table(name = "ITEM")
 @BatchSize(size = 10)
 @DiscriminatorFormula("case when ITEM_IS_SPECIAL is not null then A else B")
@@ -21,10 +27,31 @@ public class Item {
 
   private String name;
   private String description;
+
+  /**
+   * This field is settled by default to 1 during insertion in this case you have
+   * to trigger a select after an insertion or update in order to get this value
+   * but you have to keep the column updatable and insertable
+   * 
+   */
+  @Column(columnDefinition = "number(10,2) default '1'")
+  @org.hibernate.annotations.Generated(org.hibernate.annotations.GenerationTime.INSERT)
   private BigDecimal initialPrice;
   private BigDecimal reservePrice;
   private Date startDate;
   private Date endDate;
+
+  /**
+   * This field is settled by trigger in database Generation time could be always
+   * or insert, with this property hibernate will make a refresh (SELECT )
+   * immediatly after the insert or update in order to get the value
+   */
+  @Column(updatable = false, insertable = false)
+  @org.hibernate.annotations.Generated(org.hibernate.annotations.GenerationTime.ALWAYS)
+  private Date lastModified;
+
+  @Transient
+  private BigDecimal totalIncludingTax;
 
   private Set<Category> categories = new HashSet<>();
 
@@ -39,12 +66,24 @@ public class Item {
     return description;
   }
 
+  /*
+   * Hibenrate will consider the following two annotations as the same so it's
+   * better to chooese one of the two
+   */
+  // @Basic(optional = false) // This annotation will set that this method cannot
+  // return null at java level
+  @Column(nullable = false) // This annotation is responsable for NOT NULL constraint in datbase
   public BigDecimal getInitialPrice() {
     return initialPrice;
   }
 
   public BigDecimal getReservePrice() {
     return reservePrice;
+  }
+
+  @org.hibernate.annotations.Formula("TOTAL + TAX_RATE * TOTAL")
+  public BigDecimal getTotalIncludingTax() {
+    return totalIncludingTax;
   }
 
   public Date getStartDate() {
@@ -57,6 +96,10 @@ public class Item {
 
   public Set<Category> getCategories() {
     return categories;
+  }
+
+  public Date getLastModified() {
+    return lastModified;
   }
 
   public void setName(String name) {
